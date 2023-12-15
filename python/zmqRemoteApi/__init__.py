9052d7046ad2ef3bc438b0a80cb7a2fc3f502124
JorgeKGIT <jkmartin12340@gmail.com>
2023-12-15 12:39:21 +0100

    Arreglado tratamiento de posicion

--- python/zmqRemoteApi/__init__.py
+++ python/zmqRemoteApi/__init__.py
@@ -1,5 +1,7 @@
 import sys
 sys.path.append('..')
 sys.path.append('C:\\Users\\Jorge\\Desktop\\Uni\Master\\Tercer Semestre\\Robótica\\proyecto_acrobot\\python')
+sys.path.append('F:\\UNI\MASTER\\Tercer Semestre\\Robótica\\git_acrobot\\Coppelia_Acrobot\\python')
+
 from src.coppeliasim_zmqremoteapi_client import *
 print('warning: import zmqRemoteApi is deprecated. Please use import coppeliasim_zmqremoteapi_client instead.')
