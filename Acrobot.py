9f4747869c859ca3b8b082d9daf3403f044eab86
JorgeKGIT <jkmartin12340@gmail.com>
2024-01-06 18:58:14 +0100

    300 generaciones

--- Acrobot.py
+++ Acrobot.py
@@ -106,11 +106,23 @@ class Acrobot():
     def get_pos_joint1(self):
          return self.sim.getJointPosition(self.joint1_handle) * 180 / np.pi
 
+    def get_pos_joint2_rad(self):
+        return self.sim.getJointPosition(self.joint2_handle)
+
+    def get_pos_joint1_rad(self):
+         return self.sim.getJointPosition(self.joint1_handle) 
+    
     def get_pos_dummy(self):
         return self.sim.getObjectPosition(self.dummy_handle,self.sim.handle_world)
          
-    def get_velocity_joint(self):
-        return  self.sim.getJointTargetVelocity(self.joint2_handle)
+    def get_velocity_joint2(self):
+        return  self.sim.getJointVelocity(self.joint2_handle)
+    def get_velocity_joint1(self):
+        return  self.sim.getJointVelocity(self.joint1_handle)
+    
+    def get_velocity_dummy(self):
+        return  self.sim.getObjectVelocity(self.dummy_handle)
+    
     def get_torque_joint(self):
         return self.sim.getJointForce(self.joint2_handle)
     
@@ -133,7 +145,12 @@ class Acrobot():
 
 def calcular_recompensa(distance):
    
-    return (2-distance) 
+    if distance < 0.1:  # Si está muy cerca del objetivo
+        recompensa = 100  # Recompensa alta por alcanzar el objetivo
+    else:
+        recompensa = max(0, (2 - distance) * 10)
+    
+    return recompensa
 
 
 import neat
@@ -143,129 +160,128 @@ def eval_genomes(genomes, config):
     # BORRAR client = b0RemoteApi.RemoteApiClient('b0RemoteApi_pythonClient', 'b0RemoteApi') 
     coppelia = Coppelia() # Conectar a CoppeliaSim
     robot = Acrobot(coppelia.sim, 'Cuboid')
+    contador_genomas = 0
+    
     for genome_id, genome in genomes:
+        print("Numero de genoma: ",contador_genomas , " genome_id",genome_id)
         # Crear una red neuronal para el genoma
-        genome.fitness=0
+        
+        activation_default = config.genome_config.activation_default
+        #print("Función de activación predeterminada:", activation_default)
         net = neat.nn.FeedForwardNetwork.create(genome, config)
         
         total_reward = 0.0
         reward= []
-        for _ in range(3):  # Ejecutar 5 episodios para evaluar
-            # Iniciar la simulación en CoppeliaSim
-            coppelia = Coppelia() # Conectar a CoppeliaSim
-            robot = Acrobot(coppelia.sim, 'Cuboid')
-            coppelia.start_simulation()
-            robot.start_motor()
-            robot.set_joint_torque(0)  
-            
-            steps = 0
-            time_near_target = 0  # Contador de tiempo cerca del objetivo
-            estuvo_cerca=False
-            tiempo_sim=5
-            while (t := coppelia.sim.getSimulationTime()) < tiempo_sim:
-                # Calcular la distancia entre la posición actual y el objetivo
-                distance = 1-calcular_pos(robot)  # Implementa esta función
-                
-                # Calcular la recompensa en base a la cercanía al objetivo y el tiempo
-                # Aquí, puedes diseñar una función que premie el tiempo cerca del objetivo
-                '''if  (1-calcular_pos(robot)) < umbral_cercania and not estuvo_cerca:
-                    cercania_start=coppelia.sim.getSimulationTime()
-                    estuvo_cerca=True
-                    # Podría ser algo como (distancia * tiempo_cerca) / steps o alguna variante
-                    
-                else:
-                    reward=0
-                if estuvo_cerca and (1-calcular_pos(robot)) > umbral_cercania:
-                    cercania_end=coppelia.sim.getSimulationTime()
-                    time_near_target+=cercania_end-cercania_start
-                    estuvo_cerca=False
-                    reward += calcular_recompensa(distance, coppelia.sim.getSimulationTime() )  # Implementa esta función
-                '''
-                 # Implementa esta función
-                #reward.append( calcular_recompensa(distance ) )
-                if distance < 0.1:  # Si está muy cerca del objetivo
-                    recompensa = 100  # Recompensa alta por alcanzar el objetivo
-                else:
-                    recompensa = max(0, 2 - distance * 10)  # Penalización gradual mientras se aleja del objetivo
-                reward.append( recompensa )
-                # Sumar la recompensa total y actualizar el tiempo cerca del objetivo
-                
-                
-                # Ejecutar la red neuronal para obtener la acción
-                # Posicion Joint 1
-                # Posicion Joint 2
-                # Posicion Robot
-                # Distancia
-                # Fuerza
-                # Tiempo
-                tiempo_Act=coppelia.sim.getSimulationTime()
-                j1=robot.get_pos_joint1()
-                j2=robot.get_pos_joint2()
-                pos=calcular_pos(robot)
-                torque=robot.get_torque_joint()
-                if tiempo_Act is None:
-                    print("ES NONE: tiempo_Act",tiempo_Act )
-                if j1 is None:
-                    print("ES NONE: j1",j1 )
-                if j2 is None:
-                    print("ES NONE: j2",j2 )
-                if pos is None:
-                    print("ES NONE: pos",pos )
-                if torque is None:
-                    torque=robot.get_torque_joint()
-                    if torque is None:
-                        torque=torque_guardado
-                        print("ES NONEEEE: torque",torque )
-                
-                action = net.activate((j1,j2,pos,distance,torque, tiempo_Act))  # Los inputs son la distancia y el tiempo cerca del objetivo
-                #print("La accion es:", action, " con recompensa: ",calcular_recompensa(distance ))
-                #print(robot.get_pos_joint1()," ",robot.get_pos_joint2()," ",calcular_pos(robot)," ",distance," ",robot.get_torque_joint()," ", coppelia.sim.getSimulationTime())
-                robot.set_joint_torque(0.2*action[0])
-                torque_guardado=0.2*action[0]
-                # Enviar la acción a CoppeliaSim y avanzar la simulación un paso
-                # Implementa la lógica para enviar la acción y avanzar la simulación
-            
-            total_reward = sum(reward) / len(reward)
-            print("Longitud de recompensas: ",len(reward))
-            # Detener la simulación después de cada episodio
-            coppelia.stop_simulation()
-            print("Fin simulacion episodica")
+        #for _ in range(3):  # Ejecutar 5 episodios para evaluar
+        # Iniciar la simulación en CoppeliaSim
+        coppelia = Coppelia() # Conectar a CoppeliaSim
+        robot = Acrobot(coppelia.sim, 'Cuboid')
+        coppelia.start_simulation()
+        robot.start_motor()
+        robot.set_joint_torque(0)  
         
+        steps = 0
+        time_near_target = 0  # Contador de tiempo cerca del objetivo
+        estuvo_cerca=False
+        tiempo_sim=20
+        vuelta=False
+        while (t := coppelia.sim.getSimulationTime()) < tiempo_sim:
+            # Calcular la distancia entre la posición actual y el objetivo
+            distance = 1-calcular_pos(robot)  
+              # Penalización gradual mientras se aleja del objetivo
+            reward.append( calcular_recompensa(distance) )
+            if( robot.get_pos_joint2_rad()>np.pi*2 or robot.get_pos_joint2_rad()<-np.pi*2):
+                #print("Ha dado vuelta entera")
+                vuelta=True
+                break
+            # Ejecutar la red neuronal para obtener la acción
+            # Posicion
+            # Posicion Joint 1
+            # Posicion Joint 2
+            # Velocidad Joint 1 
+            # Velocidad Joint 2
+            # Tiempo
+            pos=calcular_pos(robot)
+            j1=robot.get_pos_joint1_rad()
+            j2=robot.get_pos_joint2_rad()
+            j1_dot=robot.get_velocity_joint1()
+            j2_dot=robot.get_velocity_joint2()  
+            pos_dot=robot.get_velocity_dummy()[1][2]
+            action = net.activate((pos,j1,j2,j1_dot,j2_dot, pos_dot))  # Los inputs son la distancia y el tiempo cerca del objetivo
+            #print("action= ",action)
+            #print(pos,j1,j2,j1_dot,j2_dot,robot.get_velocity_dummy()[1][2])
+            #print("{:.4f} {:.4f} {:.4f} {:.4f} {:.4f} {:.4f}".format(pos, j1, j2, j1_dot, j2_dot, pos_dot))
+            robot.set_joint_torque(0.2*action[0])
+            #torque_guardado=0.2*action[0]
+            # Enviar la acción a CoppeliaSim y avanzar la simulación un paso
+            # Implementa la lógica para enviar la acción y avanzar la simulación
+        if vuelta :
+            total_reward=-100
+        elif sum(reward)<0.05:
+            total_reward=-1000
+        else: 
+            total_reward = sum(reward) / len(reward)
+
+        print("Longitud de recompensas: ",len(reward))
+        # Detener la simulación después de cada episodio
+        coppelia.stop_simulation()
+        print("Fin simulacion episodica")
+        contador_genomas+=1
         # Calcular la aptitud del genoma basada en la recompensa total
-        genome.fitness = total_reward / tiempo_sim  # Promedio de las recompensas de los 5 episodios
-    
+        genome.fitness = total_reward   # Promedio de las recompensas de los 5 episodios
+        print("Fitness: ",genome.fitness," total_reward", total_reward , " sum(reward)", sum(reward))
     # Cerrar la conexión con CoppeliaSim después de evaluar todos los genomas
     print("Fin simulacion")
     coppelia.stop_simulation()
-    return genome.fitness
+
+
 def test_neat(genome,config):
     net=neat.nn.FeedForwardNetwork.create(genome,config)
     coppelia = Coppelia() # Conectar a CoppeliaSim
     robot = Acrobot(coppelia.sim, 'Cuboid')
+    coppelia.start_simulation()
+    robot.start_motor()
+    robot.set_joint_torque(0)  
     while (t := coppelia.sim.getSimulationTime()) < 20:
         distance=1-calcular_pos(robot)
-        action = net.activate((robot.get_pos_joint1(),robot.get_pos_joint2(),calcular_pos(robot),distance,robot.get_torque_joint(), coppelia.sim.getSimulationTime()))  # Los inputs son la distancia y el tiempo cerca del objetivo
+        pos=calcular_pos(robot)
+        j1=robot.get_pos_joint1_rad()
+        j2=robot.get_pos_joint2_rad()
+        j1_dot=robot.get_velocity_joint1()
+        j2_dot=robot.get_velocity_joint2()
+        pos_dot=robot.get_velocity_dummy()[1][2]
+        action = net.activate((pos,j1,j2,j1_dot,j2_dot, pos_dot))  # Los inputs son la distancia y el tiempo cerca del objetivo    
         robot.set_joint_torque(0.2*action[0])        
+    coppelia.stop_simulation()
 
 def run_neat(config):
     # Crear la población inicial
     p = neat.Population(config)
+    #p= neat.Checkpointer.restore_checkpoint('neat-checkpoint-49')
+    #neat-checkpoint-11
     # Añadir un report para mostrar el progreso
     p.add_reporter(neat.StdOutReporter(True))
     stats = neat.StatisticsReporter()
     p.add_reporter(stats)
     #Para guardar progreso del entrenamiento, cada x iteraciones
     p.add_reporter(neat.Checkpointer(1))
+
+    activation_functions = dir(neat.activations)
+    print("Funciones de activación disponibles:")
+    for activation_function in activation_functions:
+        print(activation_function)
     #Para cargar desde entrenamiento:
     #p= neat.Checkpointer.restore_checkpoint('neat_checkpoint-27')
     # Ejecutar NEAT (reemplaza 50 con el número de generaciones que desees)
-    winner = p.run(eval_genomes, 50)
+    winner = p.run(eval_genomes, 300)
+    print("Ganador: ",winner)
     #Best genomes:
     with open("best.pickle","wb") as f:
         pickle.dump(winner,f)
+    test_ai(config)
 
 def test_ai(config):
-    with open("best.picke","rb") as f:
+    with open("best.pickle","rb") as f:
         winner= pickle.load(f)
     test_neat(winner,config)
 #C:/Users/Jorge/Desktop/Uni/Master/Tercer Semestre/Robótica/proyecto_acrobot/
@@ -275,7 +291,7 @@ if __name__ == "__main__":
     config_path = os.path.join(local_dir,"config.txt")
     config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction,
                      neat.DefaultSpeciesSet, neat.DefaultStagnation,
-                     config_path)  # Reemplaza 'config-feedforward' con tu archivo de configuración
-    run_neat(config)
+                     config_path)  
+    #run_neat(config)
     test_ai(config)
 
